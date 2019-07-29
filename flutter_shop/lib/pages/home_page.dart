6ca72f5a4f70366378ef37c79d9fb3f19597cbe7
quadrant26 <quadrant26@163.com>
2019-07-29 18:38:41 +0800

    测试 dio

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       appBar: AppBar(title: Text('首页')),
       body: Center(child: Text('首页'),)
     );
+  }
+}
+
+void getHttp() async{
+  try{
+    Response response;
+    response = await Dio().get('https://www.easy-mock.com/mock/5d3ec93ffd577d0215ccf756/flutterShop/dabaojian');
+    return print(response);
+  }catch(e){
+    return print(e);
   }
 }
