d2ba3446398f52a84c450bc60a317b86c53d06b2
quadrant26 <quadrant26@163.com>
2019-07-30 18:41:20 +0800

    设置httpHeaders

---
+++ flutter_shop/lib/config/httpHeaders.dart
@@ -0,0 +1,17 @@
+const httpHeaders = {
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+  'Cache-Control': 'no-cache',
+  'Connection': 'keep-alive',
+  'Content-Length': '2',
+  'Content-Type': 'application/json',
+  'Cookie':
+      '_ga=GA1.2.1560037099.1564481494; _gid=GA1.2.140023020.1564481494; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1564481529; SERVERID=3431a294a18c59fc8f5805662e2bd51e|1564481532|1564481526; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1564481535',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Pragma': 'no-cache',
+  'Referer': 'https://time.geekbang.org/',
+  'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+};
