94099b3468e43c665adf27a248876b9349b4252a
quadrant26 <quadrant26@163.com>
2019-08-12 17:43:31 +0800

    打印首页数据

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,82 +7,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据...';
 
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎您来到天上人间高级会所';
-
+  @override
+  void initState() {
+    getHomePageContent().then( (val){
+      setState(() {
+       homePageContent = val.toString(); 
+      });
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('天上人间'),),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类型',
-                  helperText: '请输入你喜欢的类型'
-                ),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕')
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              )
-            ],
-          ),
-        )
+      appBar: AppBar(title: Text('百姓生活+')),
+      body: SingleChildScrollView( // 不要用 listView 混用
+        child: Text(homePageContent)
       )
     );
-  }
-
-  void _choiceAction (){
-    print('开始选择您喜欢的类型.....');
-    if ( typeController.text.toString() == ''){
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(title: Text('美女类型不能为空'))
-      );
-    }else{
-      postHttp( typeController.text.toString() ).then( (val){
-        setState(() {
-          showText = val['data']['name'].toString();
-        });
-      });
-    }
-  }
-
-  Future getHttp(String TypeText) async {
-    try{
-      Response response;
-      var data = {'name': TypeText};
-      response = await Dio().get('https://www.easy-mock.com/mock/5d3ec93ffd577d0215ccf756/flutterShop/dabaojian',
-        queryParameters: data
-      );
-      return response.data;
-    }catch(e){
-      return print(e);
-    }
-  }
-
-  Future postHttp(String TypeText) async {
-    try{
-      Response response;
-      var data = {'name': TypeText};
-      response = await Dio().post('https://www.easy-mock.com/mock/5d3ec93ffd577d0215ccf756/flutterShop/dabaojian_post',
-        queryParameters: data
-      );
-      return response.data;
-    }catch(e){
-      return print(e);
-    }
   }
 }
