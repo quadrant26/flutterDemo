b791f278506cb9e105eaa37963e5ecbe86f71763
quadrant26 <quadrant26@163.com>
2019-08-13 18:22:25 +0800

    点击拨打电话和热门推荐页

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_swiper/flutter_swiper.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -33,12 +34,21 @@ class _HomePageState extends State<HomePage> {
             var data = json.decode(snapshot.data.toString());
             List<Map> swiper = (data['data']['banner'] as List).cast();
             List<Map> navigatorList = (data['data']['subnav'] as List).cast();
+            String adPicture = data['data']['adv_click_index'];
+            String leaderImage = data['data']['cellmoile']['icon'];
+            String leaderPhone = data['data']['cellmoile']['phone'];
+            List<Map> recommendList = (data['data']['hotgoods'] as List).cast();
 
-            return Column(
-              children: <Widget>[
-                SwiperDiy(swiperDateList: swiper),
-                TopNavigator(navigatorList: navigatorList),
-              ]
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDateList: swiper),
+                  TopNavigator(navigatorList: navigatorList),
+                  AdBanner(adPicture: adPicture),
+                  LeaderPhone(leaderImage: leaderImage, leaderPhone: leaderPhone),
+                  Recommend(recommendList: recommendList)
+                ]
+              ),
             );
           }else{
             return Center(
@@ -121,3 +131,130 @@ class TopNavigator extends StatelessWidget {
 }
 
 // 首页广告条
+class AdBanner extends StatelessWidget {
+
+  final String adPicture;
+
+  AdBanner({Key key, this.adPicture}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(adPicture)
+    );
+  }
+}
+
+// 首页点击图片拨打电话
+class LeaderPhone extends StatelessWidget {
+  final String leaderImage; // 图片
+  final String leaderPhone; // 电话
+
+  LeaderPhone({Key key, this.leaderImage, this.leaderPhone}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL () async {
+    // 拨打电话
+    String url = 'tel:' + leaderPhone;
+    // 跳转到页面
+    // String url = 'http://www.baidu.com/';
+    if ( await canLaunch(url)){
+      await launch(url);
+    }else{
+      throw 'url 不能进行访问';
+    }
+  }
+}
+
+// 商品推荐类
+class Recommend extends StatelessWidget {
+  final List recommendList;
+
+  Recommend({Key key, this.recommendList}) : super(key: key);
+
+  // 商品的标题
+  Widget _titleWidget (){
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.fromLTRB(10.0, 2.0, 0, 5.0),
+      decoration: BoxDecoration(
+        color:Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 0.5, color: Colors.black12)
+        )
+      ),
+      child: Text(
+        '商品推荐', 
+        style: TextStyle(color: Colors.pink)
+      )
+    );
+  }
+
+  // 商品单独项
+  Widget _item(index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(330),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            left: BorderSide(width: 0.5, color: Colors.black12)
+          )
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['img']),
+            Text('￥${recommendList[index]['marketPrice']}'),
+            Text(
+              '￥${recommendList[index]['price']}',
+              style: TextStyle(
+                decoration: TextDecoration.lineThrough,
+                color: Colors.grey
+              )
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  // 横向列表
+  Widget _recommendList (){
+    return Container(
+      height: ScreenUtil().setHeight(350),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: recommendList.length,
+        itemBuilder: (context, index){
+          return _item(index);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(410),
+      margin: EdgeInsets.only(top: 10.0),
+      child: Column(
+        children: <Widget>[
+          _titleWidget(),
+          _recommendList()
+        ]
+      ),
+    );
+  }
+}
