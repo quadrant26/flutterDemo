eba921feabc6b6ca75dea4468109bda15ffad5bc
quadrant26 <quadrant26@163.com>
2019-08-19 18:52:51 +0800

    填充左边信息分类列表

--- flutter_shop/lib/pages/category_page.dart
+++ flutter_shop/lib/pages/category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
@@ -11,11 +12,82 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    _getCategory();
-    return Container(
-      child: Center(
-        child: Text('分类页面')
+    return Scaffold(
+      appBar: AppBar(title: Text('商品分类')),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        )
       )
     );
+  }
+}
+
+// 左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+
+  List list = [];
+
+  @override
+  void initState() {
+    _getCategory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1.0, color: Colors.black12),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index){
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  // 单独的子项
+  Widget _leftInkWell(int index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top:20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1.0, color: Colors.black12),
+          ),
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      )
+    );
+  }
+
+  void _getCategory() async {
+    var formData = {"act": "category"};
+      
+    await request('getCategory', formData: formData).then( (val){  
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
+      });
+    });
   }
 }
