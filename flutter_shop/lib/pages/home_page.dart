4db69bcf875da9a3abb2ba3c64417323fde2f1ff
quadrant26 <quadrant26@163.com>
2019-08-19 16:27:20 +0800

    physics : NeverScrollableScrollPhysics()

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -259,6 +259,7 @@ class TopNavigator extends StatelessWidget {
       height: ScreenUtil().setHeight(360),
       padding: EdgeInsets.all(3.0),
       child: GridView.count(
+        physics: NeverScrollableScrollPhysics(),
         crossAxisCount: 5,
         padding: EdgeInsets.all(5.0),
         children: navigatorList.map( (item){
