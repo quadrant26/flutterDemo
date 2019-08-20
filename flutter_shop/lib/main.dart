e508604d5a23b0db4becbc8e2b94064511bec721
quadrant26 <quadrant26@163.com>
2019-08-20 18:05:31 +0800

    使用 provide 状态管理

--- flutter_shop/lib/main.dart
+++ flutter_shop/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import './pages/index_page.dart';
+import 'package:provide/provide.dart';
+import './provide/counter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  var counter = Count();
+  var providers = Providers();
+
+  providers..provide(Provider<Count>.value(counter));
+  runApp(ProviderNode(child: MyApp(), providers: providers));
+}
 
 class MyApp extends StatelessWidget {
   @override
