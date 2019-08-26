ad5d0077c7867ecb94534d02994647270fdbb9e4
quadrant26 <quadrant26@163.com>
2019-08-26 17:01:58 +0800

    分类大类切换和小类切换高亮状态显示

--- flutter_shop/lib/provide/child_category.dart
+++ flutter_shop/lib/provide/child_category.dart
@@ -3,8 +3,12 @@ import '../model/category.dart';
 
 class ChildCategory with ChangeNotifier{
   List<BxMallSubDto> childCategoryList = [];
+  int childIndex = 0; // 子类高亮索引
 
+  // 大类切换逻辑
   getChildCategory(List<BxMallSubDto> list){
+    // 点击大类清零
+    childIndex = 0;
     // 全部
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId = "00";
@@ -14,6 +18,12 @@ class ChildCategory with ChangeNotifier{
     childCategoryList = [all];
     childCategoryList.addAll(list);
 
+    notifyListeners();
+  }
+
+  // 改变子类索引
+  changeChildIndex(index){
+    childIndex = index;
     notifyListeners();
   }
 }
