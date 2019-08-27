eae82f0febe146270d2cdec472ade85a971973db
quadrant26 <quadrant26@163.com>
2019-08-27 16:33:11 +0800

    FlutterToast 提示

--- flutter_shop/lib/pages/category_page.dart
+++ flutter_shop/lib/pages/category_page.dart
@@ -8,6 +8,7 @@ import '../model/categoryGoodsList.dart';
 import '../provide/child_category.dart';
 import '../provide/category_goods_list.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -291,6 +292,14 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
       var data = json.decode(val.toString());
       CategoryGoodsListModel goodsList = CategoryGoodsListModel.fromJson(data);
       if( goodsList.data == null){
+        Fluttertoast.showToast(
+          msg: '已经到底了',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.pink,
+          textColor: Colors.white,
+          fontSize: 16.0
+        );
         Provide.value<ChildCategory>(context).changeNoMore("没有更多了..");
       }else{
         Provide.value<CategoryGoodsListProvide>(context).getMoreGoodsList(goodsList.data);
--- flutter_shop/lib/provide/child_category.dart
+++ flutter_shop/lib/provide/child_category.dart
@@ -19,7 +19,7 @@ class ChildCategory with ChangeNotifier{
     noMoreText = '';
     // 全部
     BxMallSubDto all = BxMallSubDto();
-    all.mallSubId = "00";
+    all.mallSubId = "";
     all.mallSubName = "全部";
     all.mallCategoryId = "00" ;
     all.comments = "null";
