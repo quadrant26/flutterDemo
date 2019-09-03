ea0dbe43f6ff849afe01ad61476c2234afeb2a86
quadrant26 <quadrant26@163.com>
2019-09-03 17:09:23 +0800

    provide tabbar

--- flutter_shop/lib/provide/detail_info.dart
+++ flutter_shop/lib/provide/detail_info.dart
@@ -5,6 +5,22 @@ import 'dart:convert';
 
 class DetailInfoProvide with ChangeNotifier {
   DetailModel goodsInfo;
+  
+  bool isLeft = true;
+  bool isRight = false;
+
+  // tabbar 的切换方法
+  changeLeftAndRight(String changeState){
+    if( changeState == 'left'){
+      isLeft = true;
+      isRight = false;
+    }else{
+      isLeft = false;
+      isRight = true;
+    }
+
+    notifyListeners();
+  }
 
   // 从后台获取商品数据
   getGoodsInfo (String id){
