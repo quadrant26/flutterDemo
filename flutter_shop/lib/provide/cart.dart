8b9917b823be8b3509243967d7d9fca7fa3efc39
quadrant26 <quadrant26@163.com>
2019-09-06 18:30:17 +0800

    cart 数据模型和查找

--- flutter_shop/lib/provide/cart.dart
+++ flutter_shop/lib/provide/cart.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../model/cartInfo.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = "[]";
+  List<CartInfoModel> cartList = [];
 
   // 加入购物车
   save(goodsId, goodsName, count, price, images) async {
@@ -12,6 +14,9 @@ class CartProvide with ChangeNotifier {
     // 判断是否有值
     var temp = cartString == null ? [] : json.decode(cartString.toString());
     List<Map> tempList = (temp as List).cast();
+
+    print(temp);
+    
     // 循环判断是否有重复的
     bool isHave = false;
     int ival = 0;
@@ -19,6 +24,7 @@ class CartProvide with ChangeNotifier {
       // 有重复的
       if(item['goodsId'] == goodsId){
         tempList[ival]['count'] = item['count']+1;
+        cartList[ival].count++;
         isHave = true;
       }
       ival++;
@@ -26,27 +32,47 @@ class CartProvide with ChangeNotifier {
 
     // 没有插入数据
     if(!isHave){
-      tempList.add({
+      Map<String, dynamic> newGoods = {
         'goodsId': goodsId, 
         'goodsName': goodsName, 
         'count': count, 
         'price': price, 
         'images': images
-      });
+      };
+      tempList.add(newGoods);
+      cartList.add(CartInfoModel.fromJson(newGoods));
     }
 
     // 数据静态化
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串>>>>>>>>${cartString}');
+    print('数据模型>>>>>>>>>>>>>>>${cartList}');
     prefs.setString('cartInfo', cartString);
+    
     notifyListeners();
   }
 
   // 清空购物车
   remove () async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    cartString = prefs.getString('cartInfo');
+    prefs.remove('cartInfo');
+    cartList = [];
     print('清空完成----------------------');
+    notifyListeners();
+  }
+
+  getCartInfo () async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList = [];
+    if ( cartString == null){
+      cartList = [];
+    }else{
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
